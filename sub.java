b84c8b7796941181f6bf1bf2bb9658c451d1b867
saikumardestiny <32240771+saikumardestiny@users.noreply.github.com>
2020-02-20 17:17:06 +0530

    first commit for subtraction

---
+++ sub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java .io.*;
+public class Arithmetic{
+        public static void main(String ar[]){
+                public void Addition(int a,int b){
+                        
+                        a=90;
+                        b=8;
+                        System.out.println(a-b);
+                }
+        }
+}
